c549ab7373392a3351005a92ff976825b01ede18
Manu <manel.gf.95@gmail.com>
2024-11-26 01:06:21 +0100

    add flowbite svelte + test tooltip

---
+++ src/routes/map/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { Tooltip, Button } from 'flowbite-svelte';
+</script>
+
+<div class="w-5 h-5 pt-20">
+
+</div>
+
+<Button class="ml-10" id="placement">+</Button>
+<Tooltip triggeredBy="#placement" placement='bottom'>Here lies the body of deez nuts</Tooltip>
